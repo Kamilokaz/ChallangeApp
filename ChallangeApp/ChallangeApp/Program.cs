3d0b9013bd57a421a8e2f23bc721a35c9073dc78
Kamil Okaz <kamil.okaz1413@gmail.com>
2023-04-15 20:28:45 +0200

    Stworzenie pustej aplikacji

---
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿String name = "Kamil";
+bool male = true;
+var age = 30;
+string result1 = "Mężczyzna poniżej 30lat";
+string result2 = "Kamil, lat 30";
+string result3 = "Niepełnoletnia Kobieta";
+
+
+if (age < 30 && male == true)
+{
+    Console.WriteLine( result1 );
+}
+if (age == 30 && male == true && name == "Kamil" )
+{
+    Console.WriteLine( result2 );
+}
+if (age < 18 && male == true)
+{
+    Console.WriteLine( result3 );
+}
